25184198e4df1d679b4f5e15dcf73134c08cf767
Aniyeshu Verma <99887733+Aniyeshu-31@users.noreply.github.com>
2024-11-17 20:34:15 +0530

    Updated Backend Code

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,48 +1,116 @@
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
-export const registerUser= async(req,res,next)=>{
+
+// Register User
+export const registerUser = async (req, res, next) => {
     const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(req.body.password,salt); 
-       try {
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    try {
         const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:hashedPassword,
-            photo:req.body.photo
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+            photo: req.body.photo,
         });
 
         await newUser.save();
-        res.status(200).json({success:true,message:'Successfully registered!'});
+        res.status(200).json({ success: true, message: 'Successfully registered!' });
     } catch (err) {
-        res.status(500).json({success:false,message:'Unable to register the User!!'});
+        console.error('Registration Error:', err);
+        res.status(500).json({ success: false, message: 'Unable to register the user!' });
     }
-}
-export const LoginUser = async(req,res,next)=>{
+};
+
+// Login User
+export const LoginUser = async (req, res, next) => {
     try {
         const email = req.body.email;
-       const user = await User.findOne({email});
-       if(!user){
-        res.status(404).json({success:false ,message:'User not Found!!'});
-       }
-       const checkpassword = await bcrypt.compare(req.body.password,user.password);
 
-       if(!checkpassword){
-        return res.status(401).json({success:false,message:'User password is incorrect'});
-       }
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found!' });
+        }
 
-       const {password,role,...rest}=user._doc;
+        // Check password
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(401).json({ success: false, message: 'Incorrect password!' });
+        }
 
-       const token = jwt.sign({id:user._id,role:user.role}, process.env.JWT_SECRET_KEY,{expiresIn:'15d'});
+        // Destructure user object excluding sensitive information
+        const { password, role, ...rest } = user._doc;
 
-       // set token in browser cookies and send it to the client in the response
+        // Generate JWT token
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: '15d' }
+        );
 
-       res.cookie('accessToken',token,{
-        httpOnly:true,
-        expiresIn:token.expiresIn
-       }).status(200).json({success:true,message:'LoggedIn Successfully!!' ,data:{...rest}});
+        // Set token in browser cookies and send response
+        res.cookie('accessToken', token, {
+            httpOnly: true,
+            maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
+        });
+
+        res.status(200).json({
+            success: true,
+            message: 'Logged in successfully!',
+            data: { ...rest },
+        });
     } catch (err) {
-        console.log(err);
-        return res.status(401).json({success:false,message:'Failed To Login'});
-    }
-}
+        console.error('Login Error:', err);
+        res.status(500).json({ success: false, message: 'Failed to log in!' });
+    }
+};
+// import User from '../models/User.js';
+// import jwt from 'jsonwebtoken';
+// import bcrypt from 'bcryptjs';
+// export const registerUser= async(req,res,next)=>{
+//     const salt = bcrypt.genSaltSync(10);
+//     const hashedPassword = bcrypt.hashSync(req.body.password,salt); 
+//        try {
+//         const newUser = new User({
+//             username:req.body.username,
+//             email:req.body.email,
+//             password:hashedPassword,
+//             photo:req.body.photo
+//         });
+
+//         await newUser.save();
+//         res.status(200).json({success:true,message:'Successfully registered!'});
+//     } catch (err) {
+//         res.status(500).json({success:false,message:'Unable to register the User!!'});
+//     }
+// }
+// export const LoginUser = async(req,res,next)=>{
+//     try {
+//         const email = req.body.email;
+//        const user = await User.findOne({email});
+//        if(!user){
+//         res.status(404).json({success:false ,message:'User not Found!!'});
+//        }
+//        const checkpassword = await bcrypt.compare(req.body.password,user.password);
+
+//        if(!checkpassword){
+//         return res.status(401).json({success:false,message:'User password is incorrect'});
+//        }
+
+//        const {password,role,...rest}=user._doc;
+
+//        const token = jwt.sign({id:user._id,role:user.role}, process.env.JWT_SECRET_KEY,{expiresIn:'15d'});
+
+//        // set token in browser cookies and send it to the client in the response
+
+//        res.cookie('accessToken',token,{
+//         httpOnly:true,
+//         expiresIn:token.expiresIn
+//        }).status(200).json({success:true,message:'LoggedIn Successfully!!' ,data:{...rest}});
+//     } catch (err) {
+//         console.log(err);
+//         return res.status(401).json({success:false,message:'Failed To Login'});
+//     }
+// }
