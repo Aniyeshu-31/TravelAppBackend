bfe60106f74f01cb8ebc6f6cadcf0b0a14127ae0
Aniyeshu <202152304@iiit.vadodara.ac.in>
2025-03-24 19:34:13 +0530

    IntegratedPayment System

--- routes/booking.js
+++ routes/booking.js
@@ -1,10 +1,15 @@
-import express from 'express';
-import { createbooking, getAllBookingDetails, getBookingDetails } from '../controllers/bookingController.js';
-import { verifyUser,verifyAdmin } from '../Utils/verifyToken.js';
-const router= express.Router();
+import express from 'express'
+import {
+  confirmBooking,
+  createbooking,
+  getAllBookingDetails,
+  getBookingDetails,
+} from '../controllers/bookingController.js'
+import { verifyUser, verifyAdmin } from '../Utils/verifyToken.js'
+const router = express.Router()
 
-router.post('/',createbooking)
-router.get('/:id',verifyUser,getBookingDetails)
-router.get('/',verifyAdmin,getAllBookingDetails)
-
-export default router;
+router.post('/', verifyUser, createbooking)
+router.get('/:id', verifyUser, getBookingDetails)
+router.get('/', verifyAdmin, getAllBookingDetails)
+router.get('/verify-payment', verifyAdmin, confirmBooking)
+export default router
